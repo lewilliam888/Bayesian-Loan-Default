331122a2fb8976b8c2b16d6bd60afbda319017c3
William Le <150302246+lewilliam888@users.noreply.github.com>
2026-04-28 21:23:38 -0400

    Add batch_run.sh for job submission script

---
+++ batch_run.sh
@@ -0,0 +1,13 @@
+cat > batch_run.sh << 'EOF'
+#!/bin/tcsh
+#BSUB -J dsa595_sim[1-200]
+#BSUB -n 1
+#BSUB -R span[hosts=1]
+#BSUB -W 00:30
+#BSUB -o output/logs/sim_%J_%I.out
+#BSUB -e output/logs/sim_%J_%I.err
+
+module load R
+
+Rscript run_file.r $LSB_JOBINDEX output/traces/
+EOF
